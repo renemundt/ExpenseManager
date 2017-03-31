352c0b55e29e6c2d53080ba16a4448300cfc49a8
René Mundt <renem@live.dk>
2017-03-31 20:02:38 +0200

    fix(last day of month): include last day in month in average calculation

--- src/app/expenses/expenses.service.ts
+++ src/app/expenses/expenses.service.ts
@@ -22,7 +22,7 @@ export class ExpensesService {
         let currentYear = date.getFullYear()
 
         let startKey = `"${moment().format('YYYY')}-${moment().format('MM')}-01T00:00:00Z"`
-        let endKey = `"${moment().format('YYYY')}-${moment().format('MM')}-${moment().daysInMonth()}T00:00:00Z"`
+        let endKey = `"${moment().format('YYYY')}-${moment().format('MM')}-${moment().daysInMonth()}T23:59:59Z"`
 
         const url = `${environment.url}/_design/timestamp/_view/expenses-view?startkey=${startKey}&endkey=${endKey}&include_docs=true`;
         
