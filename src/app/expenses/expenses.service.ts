1060cc96c663f4b39f10962e1a44a9c5f12c3234
René Mundt <renem@live.dk>
2017-09-30 21:23:37 +0200

    fix(import): map from rxjs needed to make ng build -prod work

--- src/app/expenses/expenses.service.ts
+++ src/app/expenses/expenses.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
 import { Expense } from './expense.models';
 import { Http, Response, Headers } from '@angular/http';
 
